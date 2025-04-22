d0f78481cdf1303730effc97c714f21ad8d449b2
thana <tnkmunich@gmail.com>
2025-04-22 16:12:00 +0700

    update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -8,6 +8,6 @@ test('getDay returns the long-format day of the week', () => {
 });
 test('getMonth returns the long-format month of the year', () => {
     const month = monthOfTheYear( new Date('3/11/2020') );
-    expect( month ).toBe('March');
+    expect( month ).toBe('November');
     // expect( month ).not.toBe('January');
 });
