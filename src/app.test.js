21e015b10b5535f937650d1890160bc50c38a80b
thana <tnkmunich@gmail.com>
2025-04-22 15:57:38 +0700

    update app.js and app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -4,4 +4,10 @@ const { dayOfTheWeek } = require('./app');
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
+    // expect( day ).not.toBe('Monday');
+});
+test('getMonth returns the long-format month of the year', () => {
+    const month = monthOfTheYear( new Date('3/11/2020') );
+    expect( month ).toBe('March');
+    // expect( month ).not.toBe('January');
 });
